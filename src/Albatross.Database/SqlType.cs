78b0a1fc19cba9179fe06a5c9aa98b5556d36935
Rushui Guan <rushui@outlook.com>
2018-05-04 15:27:14 -0400

    Implement IsEquatable interface for SqlType class

--- src/Albatross.Database/SqlType.cs
+++ src/Albatross.Database/SqlType.cs
@@ -6,7 +6,7 @@ namespace Albatross.Database {
 	/// <summary>
 	/// The class represents a database data type
 	/// </summary>
-	public class SqlType {
+	public class SqlType : IEquatable<SqlType> {
 		public string Schema { get; set; }
 		public string Name { get; set; }
 		public int? MaxLength { get; set; }
@@ -15,5 +15,20 @@ namespace Albatross.Database {
 		public bool IsNullable { get; set; }
 		public bool IsUserDefined { get; set; }
 		public bool IsTableType { get; set; }
+
+		public bool Equals(SqlType other) {
+			if (other != null) {
+				return Schema == other.Schema &&
+					Name == other.Name &&
+					MaxLength == other.MaxLength &&
+					Precision == other.Precision &&
+					Scale == other.Scale &&
+					IsNullable == other.IsNullable &&
+					IsUserDefined == other.IsUserDefined &&
+					IsTableType == other.IsTableType;
+			} else {
+				return false;
+			}
+		}
 	}
 }

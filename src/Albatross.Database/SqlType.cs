161c0a8ea5a0d23783b5ae004d549f95792fc2b8
Rushui Guan <rushui@outlook.com>
2018-05-04 14:56:30 -0400

    Make SqlType.Precision, Scale and MaxLength nullable

--- src/Albatross.Database/SqlType.cs
+++ src/Albatross.Database/SqlType.cs
@@ -9,9 +9,9 @@ namespace Albatross.Database {
 	public class SqlType {
 		public string Schema { get; set; }
 		public string Name { get; set; }
-		public int MaxLength { get; set; }
-		public int Precision { get; set; }
-		public int Scale { get; set; }
+		public int? MaxLength { get; set; }
+		public int? Precision { get; set; }
+		public int? Scale { get; set; }
 		public bool IsNullable { get; set; }
 		public bool IsUserDefined { get; set; }
 		public bool IsTableType { get; set; }

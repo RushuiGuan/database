5055126507a9f96009c0c8e6eb713fc517837647
Rushui Guan <rushui@outlook.com>
2018-10-16 23:07:14 -0400

    update ParseCriteria

--- src/Albatross.Database.SqlServer/ParseCriteria.cs
+++ src/Albatross.Database.SqlServer/ParseCriteria.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Albatross.Database.SqlServer
-{
+namespace Albatross.Database.SqlServer{
+	/// <summary>
+	/// convert wild card * to sql wild card
+	/// </summary>
 	public class ParseCriteria : IParseCriteria {
 		public void Parse(string criteria, out string schema, out string name) {
 			schema = null;
@@ -11,11 +13,21 @@ namespace Albatross.Database.SqlServer
 			if (!string.IsNullOrEmpty(criteria)) {
 				int index = criteria.IndexOf('.');
 				if (index == -1) {
-					name = criteria;
+					name = criteria.Trim();
 				} else {
-					schema = criteria.Substring(0, index);
-					name = criteria.Substring(index + 1);
+					schema = criteria.Substring(0, index).Trim();
+					name = criteria.Substring(index + 1).Trim();
 				}
+			}
+			if(name == "*") {
+				name = null;
+			}else if (!string.IsNullOrEmpty(name)) {
+				name = name.Replace("*", "%");
+			}
+			if (schema == "*") {
+				schema = null;
+			} else if (!string.IsNullOrEmpty(schema)) {
+				schema = schema.Replace("*", "%");
 			}
 		}
 	}
